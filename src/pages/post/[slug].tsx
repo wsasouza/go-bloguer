83e1d4772dbcd53349b908f0181cf8d78beee7ce
wsasouza <wsasouza@hotmail.com>
2022-03-11 16:29:10 -0300

    Feat: Building the Post Page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next'
+import PortableText from 'react-portable-text'
 
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
@@ -9,10 +10,59 @@ interface Props {
 }
 
 function Post({ post }: Props) {
-  console.log(post)
   return (
     <main>
       <Header />
+
+      <img
+        src={urlFor(post.mainImage).url()!}
+        alt="mainImage"
+        className="h-40 w-full object-cover"
+      />
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            src={urlFor(post.author.image).url()!}
+            alt="authorImage"
+            className="h-10 w-10 rounded-full"
+          />
+          <p className="text-sm font-extralight">
+            Blog postado por{' '}
+            <span className="font-bold text-blue-600">{post.author.name}</span>{' '}
+            - publicado em {new Date(post._createdAt).toLocaleDateString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ children, href }: any) => (
+                <a href={href} className="text-blue-600">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
     </main>
   )
 }
